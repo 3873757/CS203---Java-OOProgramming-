700118f3cffeef9d44e472a9823ed85e174746d8
3873757 <dendir_fun@yahoo.com>
2015-02-24 16:29:31 -0600

    new version

--- W1D2/Day2/src/day2/Movie.java
+++ W1D2/Day2/src/day2/Movie.java
@@ -1,11 +1,23 @@
-
 package day2;
-public class Movie 
-{
-  String tiltle;
-  String genre;
-  int rating;
-  void playIt(){
-  System.out.println("Playing the moie");
-}
+
+public class Movie {
+
+    String title;
+    String genre;
+    int rating;
+
+    public Movie(String title, String genre, int rating) {
+
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    public String toString() {
+        return "Title of the Movie is:  " + title + " Genre of the movie is : " + genre + "  Rate of the movie is : " + rating;
+    }
+
+    void playIt() {
+        System.out.println("Playing the moie");
+    }
 }
--- W1D2/Day2/src/day2/MovieTestDrive.java
+++ W1D2/Day2/src/day2/MovieTestDrive.java
@@ -4,25 +4,27 @@
  */
 package day2;
 
-public class MovieTestDrive 
-{
-    public static void main(String[] args){
-     Movie one = new Movie();
-     one.tiltle ="Gone with the stuck";
-     one.genre = "Tragic";
-     one.rating = 8;
-     one.playIt();
-     
-     Movie two =new Movie();
-     two.genre ="Cmedy";
-     two.tiltle="Lost in cubicle space";
-     two.rating= 5;
-     two.playIt();
-     
-     Movie three = new Movie();
-     three.tiltle ="Byte Club";
-     three.genre ="Tragic But ultimately uplifting";
-     three.rating = 127;
-     three.playIt();
+public class MovieTestDrive {
+
+    public static void main(String[] args) {
+        Movie one = new Movie("Gone with the stuck", "  Tragic", 8);
+        //one.tiltle ="Gone with the stuck";
+        //one.genre = "Tragic";
+        //one.rating = 8;
+        //one.playIt();
+        Movie two = new Movie("Cmedy", "  Lost in cubicle space", 5);
+        // two.genre ="Cmedy";
+        // two.tiltle="Lost in cubicle space";
+        //two.rating= 5;
+        //two.playIt();
+
+        Movie three = new Movie("Byte Club", " Byte Club", 127);
+        //three.tiltle ="Byte Club";
+        //three.genre ="Tragic But ultimately uplifting";
+        // three.rating = 127;
+        //three.playIt();
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
     }
 }
