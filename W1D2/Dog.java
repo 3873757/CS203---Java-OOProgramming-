a010a20e041cafc3734cd33991ad812c90d6739c
3873757 <dendir_fun@yahoo.com>
2015-02-24 14:21:02 -0600

    Dog Application

---
+++ W1D2/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+	String name;
+	int size;
+	String breed;
+	void bark() {
+		System.out.println("Ruff!!! Ruff!!!");
+		}
+		}
+
+
---
+++ W1D2/DogTester.java
@@ -0,0 +1,8 @@
+public	class DogTester{
+	 public static void main(String[] args) {
+		Dog d = new Dog();
+		d.name = "Pooney";
+		d.size = 23;
+		d.bark();
+	}
+}
