5ef8a8e813c2cc098ca84c5c9c2efcc9e913bafa
3873757 <dendir_fun@yahoo.com>
2015-02-25 16:15:54 -0600

    SumOfEven

---
+++ W1D2/Leccture/src/leccture/MutabilityTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leccture;
+
+/**
+ *
+ * @author jambo
+ */
+public class MutabilityTest {
+    public static void main(String[] args){
+        Dog d1 new dog();
+    }
+}
