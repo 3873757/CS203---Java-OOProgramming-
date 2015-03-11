8c33321bf7b6ee4dafda462ee0cf6461d28ae2df
3873757 <dendir_fun@yahoo.com>
2015-03-11 02:00:59 -0500

    MusicVideoPlayer

---
+++ w2d5/W3/W3D3/MiniPlayer.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w3;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+//import w3.MiniMusicPlayer.MiniMusicPlayer.MyDrawPanel;
+
+/**
+ *
+ * @author jambo
+ */
+public class MiniPlayer {
+
+    private static JFrame f = new JFrame("My First Music Video");
+    private static MyDrawPanel ml;
+
+    public static void main(String[] args) {
+        MiniPlayer mini = new MiniPlayer();
+        mini.go();
+
+    }
+
+    public void setUpGui() {
+        ml = new MyDrawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30, 30, 300, 300);
+        f.setVisible(true);
+    } // close method
+
+    public void go() {
+        setUpGui();
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.addControllerEventListener(ml, new int[]{127});
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+            int r = 0;
+            for (int i = 0; i < 60; i += 4) {
+                r = (int) ((Math.random() * 50) + 1);
+                track.add(makeEvent(144, 1, r, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
+                track.add(makeEvent(128, 1, r, 100, i + 2));
+            } // end loop
+            sequencer.setSequence(seq);
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (MidiUnavailableException | InvalidMidiDataException ex) {
+        }
+    } // close method
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) {
+        }
+        return event;
+    }
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
+
+        private boolean msg = false;
+
+        @Override
+        public void controlChange(ShortMessage event) {
+            msg = true;
+            repaint();
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            if (msg) {
+                Graphics2D g2 = (Graphics2D) g;
+                int r = (int) (Math.random() * 250);
+                int gr = (int) (Math.random() * 250);
+                int b = (int) (Math.random() * 250);
+                g.setColor(new Color(r, gr, b));
+                int ht = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 10);
+                g.fillRect(x, y, ht, width);
+                msg = false;
+            } 
+        } 
+    } 
+} 
+
